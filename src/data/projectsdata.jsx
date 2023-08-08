eede772ae26d16dd9e818d2729d253b18a02618a
npminit-dev <jorge_zetagraff@hotmail.com>
2023-08-08 06:29:40 -0300

    prueba

--- src/data/projectsdata.jsx
+++ src/data/projectsdata.jsx
@@ -1,28 +1,32 @@
 export const projectsdata = [
   {
     name: 'BookNexus',
-    info: 'prueba tecnica de @midudev',
+    info: 'Web de libros con React/TS/Ant-Design (prueba tecnica de @midudev)',
     link: 'https://dreamy-vacherin-eba168.netlify.app/',
     repo: 'https://github.com/npminit-dev/npminit-dev.git'
   },
   {
-    name: 'BS5 Landing Page',
-    info: 'Web landing page con Bootstrap5',
-    link: 'https://capable-kringle-1fe2b3.netlify.app/'
+    name: 'Landing Page',
+    info: 'Landing page con Bootstrap5',
+    link: 'https://capable-kringle-1fe2b3.netlify.app/',
+    repo: 'https://github.com/npminit-dev/BSLandingPage.git'
   },
   {
-    name: 'BS5 CRUD',
-    info: 'CRUD de equipos de futbol con Bootstrap5',
-    link: 'https://melodious-licorice-c983f3.netlify.app/'
+    name: 'CRUD equipos de futbol',
+    info: 'CRUD de equipos de futbol con React y Bootstrap5',
+    link: 'https://melodious-licorice-c983f3.netlify.app/',
+    repo: 'https://github.com/npminit-dev/BS5-React-CRUD.git'
   },
   {
-    name: 'Vainilla CRUD',
+    name: 'CRUD',
     info: 'CRUD con VainillaJS y IndexedDB',
-    link: 'https://fluffy-snickerdoodle-8f13a1.netlify.app/'
+    link: 'https://fluffy-snickerdoodle-8f13a1.netlify.app/',
+    repo: 'https://github.com/npminit-dev/Vainilla-CRUD.git'
   },
   {
     name: 'RealTime Chat',
-    info: 'Chat en tiempo real entre 2 pestañas con Service Workers',
-    link: 'https://kaleidoscopic-muffin-fe5fc9.netlify.app/'
+    info: 'Chat en tiempo real entre 2 pestañas con Vainilla y Service Workers',
+    link: 'https://kaleidoscopic-muffin-fe5fc9.netlify.app/',
+    repo: 'https://github.com/npminit-dev/RealTimeChat.git'
   }
 ]
