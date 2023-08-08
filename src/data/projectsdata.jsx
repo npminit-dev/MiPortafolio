7232d1ab514ddb623722928fe5051339866d4e79
npminit-dev <jorge_zetagraff@hotmail.com>
2023-08-08 05:28:36 -0300

    Agregado proyecto

---
+++ src/data/projectsdata.jsx
@@ -0,0 +1,28 @@
+export const projectsdata = [
+  {
+    name: 'BookNexus',
+    info: 'prueba tecnica de @midudev',
+    link: 'https://dreamy-vacherin-eba168.netlify.app/',
+    repo: 'https://github.com/npminit-dev/npminit-dev.git'
+  },
+  {
+    name: 'BS5 Landing Page',
+    info: 'Web landing page con Bootstrap5',
+    link: 'https://capable-kringle-1fe2b3.netlify.app/'
+  },
+  {
+    name: 'BS5 CRUD',
+    info: 'CRUD de equipos de futbol con Bootstrap5',
+    link: 'https://melodious-licorice-c983f3.netlify.app/'
+  },
+  {
+    name: 'Vainilla CRUD',
+    info: 'CRUD con VainillaJS y IndexedDB',
+    link: 'https://fluffy-snickerdoodle-8f13a1.netlify.app/'
+  },
+  {
+    name: 'RealTime Chat',
+    info: 'Chat en tiempo real entre 2 pestañas con Service Workers',
+    link: 'https://kaleidoscopic-muffin-fe5fc9.netlify.app/'
+  }
+]
