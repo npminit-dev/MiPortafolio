db0b04af150e7a534a090e6349c7ee78bffe6abd
Jorge Balsamo <jorgeb.dev.acc@gmail.com>
2025-12-16 20:10:15 -0300

    Fixed ES Module incompatible vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from "@tailwindcss/vite";
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@views': path.resolve(__dirname, 'src/views'),
-      '@composables': path.resolve(__dirname, 'src/composables'),
-      '@stores': path.resolve(__dirname, 'src/stores'),
-      '@types': path.resolve(__dirname, 'src/types'),
-      '@data': path.resolve(__dirname, 'src/data')
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@data': fileURLToPath(new URL('./src/data', import.meta.url))
     }
   }
 })
